8afddcad37f2148530d2dd0b27419886bcb8f716
Steve Tuckner <steve@coderow.com>
2011-05-29 22:43:46 -0500

    Updated for somewhat better display of schedules

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ActiveRecord::Base
   belongs_to :scheduleable, :polymorphic => true
-  has_many :game_roles
+  has_many :game_roles, :dependent => :destroy
   has_many :users, :through => :game_roles
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 
   def opponent(user)
     raise "more than two players for this game" unless game_roles.size == 2
@@ -13,6 +13,26 @@ class Game < ActiveRecord::Base
         return opponents.first
     else
       raise "user is not a player in this game"
+    end
+  end
+
+  def white_won
+    result == "1-0" || result == "0.5-0.5"
+  end
+
+  def black_won
+    result == "0-1" || result == "0.5-0.5"
+  end
+
+  def result_for_user(user)
+    if result == "1-0" then
+      (user == white_player) ? "Win" : "Loss"
+    elsif result == "0-1" then
+      (user == black_player) ? "Win" : "Loss"
+    elsif result == "0.5-0.5" then
+      "Tie"
+    else
+      ""
     end
   end
 
